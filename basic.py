05c7c6767ca9e4e0acd75bf231e1ecfa23527b29
Bhumika <160400051+bhumika2007@users.noreply.github.com>
2024-04-22 17:59:54 +0530

    commit basic project

---
+++ basic.py
@@ -0,0 +1,31 @@
+print("Hello World")
+print(45)
+print("Hii bhumika are you excited")
+print(45.0)
+print(type(45.0))
+a = 10
+b = 20
+#using third variable
+temp = a 
+a = b 
+b =temp
+print("a after swapping:", a)
+print("b after swapping:", b)
+a = 30
+b = 40
+#bitwise operator
+a = a + b
+b = a - b
+a = a - b
+print("a after swapping:", a)
+print("b after swapping:", b)
+# Python code to swap two numbers
+# without using another variable
+x = 5
+y = 7
+print ("Before swapping: ")
+print("Value of x : ", x, " and y : ", y)
+# code to swap 'x' and 'y'
+x, y = y, x
+print ("After swapping: ")
+print("Value of x : ", x, " and y : ", y)
