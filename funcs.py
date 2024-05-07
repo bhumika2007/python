25304e07c523cb79d43c213d3e7008ad2e7e28ba
Bhumika <160400051+bhumika2007@users.noreply.github.com>
2024-05-07 17:15:07 +0530

    questions.py

---
+++ funcs.py
@@ -0,0 +1,38 @@
+# 1.what is the purpose of function in python?
+
+# function allow you to break down your program into smaller,manageable pieces of code that can be reused and 
+# called whenever needed. they promote code reusability and readability.
+
+# 2.how do you define a function in python?
+
+# you define a function using the def keyword followed by the function name and parameters ,enclosed in parentheses.
+# the function body is indented below the definition.
+
+# 3.what is parameter and argument in a function?
+
+# parameter are the variables listed inside the parentheses in the function definition ,while argument 
+# are the values passed to the function when it is called.
+
+# 4.what is the difference between the return and print in a function?
+
+# return is used to send a value back to the caller of the function and terminates the function's execution,
+# while print is used to display a value to the console but doesnot affect the functions output or behavior.
+
+# 5.can a function return multiple values in python?
+
+# yes,a function can return multiple values in python by returning them as a tuple,list,or dictionary.
+
+# 6.can a function be called inside another function ?
+
+# yes,function can call other functions,either from the same module or from different modules.
+
+# 7.what is a recursive function?
+
+# a recursive function is a function that calls itself directly or indirectly in order to solve a problem.
+# it usually contains a base case to terminate the recursion..
+
+# 8.can functions in python have default arguments?
+
+# yes,function in python can have default arguments,which are used when the caller doesnot provide a value 
+# for that argument.
+    
